29d80187b6b00db38ec49325999d5323f8506a5f
Kian Sweeney <73000700+kiansweeney11@users.noreply.github.com>
2022-04-23 19:09:26 +0100

    push-code

---
+++ README.md
@@ -0,0 +1,118 @@
+# Agent Based Model - Virus Spread
+
+## Introduction
+This repository contains all the relevant code used to simulate a virus spread in a population and the effects different vaccination rates can have on a population, the number of reinfections that can occur and the number of deaths that can occur at varying levels of population immunity/vaccination. It is visualized and run using the PyCx framework found [here](https://github.com/hsayama/PyCX).
+
+## Running the Simulation
+To run the simulation we firstly clone this repository. We then change directory to this repo we just cloned and simply run the command:
+
+```
+python "assignment2-simulator.py"
+```
+
+This will then begin the simulation like all PyCx frameworks, with a tab for running and stopping the simulations alongside a visual of the agents and a plot of the variables we are tracking like so:
+
+![](images/sim-image.PNG)
+
+We can change the increments our model takes or close the window entirely to end the simulation. We can also adjust default parameters using the parameters tab on our PyCx simulator tab to start or stop simulations.
+
+## Model Description
+
+The following packages are required to run our model:
+
+- Pylab
+- PyCx
+- re
+- random (for sample of our population to move at each step)
+
+For our simulation we are tracking multiple states. These are:
+
+- ***healthy*** - This person is yet to be exposed to the virus and is not vaccinated / their vaccine efficacy has waned.
+- ***vaccinated*** - This person has received the vaccine and is immune to the virus for a set amount of time as a result.
+- ***immune*** - This person has been infected by the virus but has survived and now has immunity from the virus for a period of time.
+- ***recovered*** - This person has had the virus and their immunity has since waned or they failed to develop immunity to the virus. In danger of being reinfected again but can be vaccinated now that their natural immunity is gone.
+- ***infected*** - This person is actively infected by the virus and is in danger of spreading it to other agents who have no immunity or are not vaccinated.
+- ***reinfected*** - This person has had the virus, their immunity has waned and they have now been reinfected by the virus. Less to die as a result of catching the virus again. They will have long lasting immunity if they survive.
+- ***deaths*** - Here we track the number of agents who have died after contracting the virus.
+
+The parameters for each agent are as follows:
+
+- ***state*** - these are all outlined above (with exception of deaths) and change based on rules.
+- ***immunity_time*** - the remaining amount of immunity (in steps) an agent has after surviving infection. Set as -1 if not actively infected.
+- ***vaccine_efficacy*** - the amount of time an agent's vaccine is effective for before they are prone to infection again. Again, set to -1 if not applicable.
+- ***infected_time*** - the amount of time an agent is infected for. Once this value hits 14 they are immune or recovered. Once again, set to -1 for non applicable agent states.
+- ***x*** - the x co-ordinate of an agent with respect to the grid. Between 0 and 1.
+- ***y*** - the y co-ordinates of an agent with respect to the grid. Between 0 and 1.
+
+## Infection Rules
+
+We start off with a population of 1000 agents and only a small selection of the population infected. The rules of infection are based on boundary detection. Firstly, we assign a radius that the agent would be prone to infection from if they encounter another agent. If the agents neighbours are within this boundary and this neighbour is infected then this agent also becomes infected. An agent can be infected for up to 14 steps. Within this timeframe they are still infectious to other agents they encounter. Once an agent's infection time is up there is a random chance of death and hence removal of this agent from the simulation. Otherwise, they develop immunity from the virus based on a random probability or are labelled as recovered and are hence vulnerable to infection again.
+
+Agents can also become reinfected. If an agent has survived the virus and the immunity they developed from the virus has ceased to exist they can become infected again if they encounter an agent who is infected or reinfected and is within their radius for collision detection. Reinfected agents are much more prone to dying from the virus but if they survive they will have long lasting immunity as per the rules of the simulation. This attempts to mimic real-world virus infections where people can be reinfected again within a short timeframe but the chances of a third reinfection in a short period of time are remote. There is also a small chance of infection despite being vaccinated. If a vaccinated agent encounters an infected / reinfected agent they can be prone to infection but this has a small probability (this probability is adjustable but will be left very small to start).
+
+## Movement Rules and Vaccination Rules
+
+For our movement rules agents the movement is based on a uniform distribution. A random movement in the distribution boundary between +- our magnitude of movement (community_spread) is is taken and if values for x or y are above 1 for these the respective value is then labelled as 1. A random sample of agents is taken to move at each step. We take a random sample from half the size of the total population and within the range of this value we move a random number of numbers accordingly. This again mimics real world behaviour as not all agents (people) will be moving all the time throughout the stages of a virus spread like so. Vaccination rules follow a similar pattern. A randomly selected portion of the population who are either healthy or recovered from the virus are vaccinated and now have longer lasting immunity to the virus. As mentioned earlier, there is a slight chance they can get infected by the virus by their vaccine will protect them in most instances.
+
+## Scenarios / Experiments
+
+There was a selection of ideas I wanted to investigate in our virus spread simulation here. These included:
+
+- the effects of a high vaccination rate in a population on preventing deaths / reinfections.
+- the effects a low vaccination rate can have on a population's infections and deaths.
+- the impact an effective vaccine has on preventing spread of the virus and vice versa.
+
+We would model these scenarios from 0 to 100 steps and check the plots of our graphs over time at these instances. Firstly, let's look at a low vaccination uptake in the population and the effects it has on population deaths, infections and reinfections.
+
+### Experiment 1 - Low Vaccination Rate
+
+The parameters we ran for this experiment starting off were as follows:
+
+- total_population = 1000
+- infections = 10
+- probability_infection = 0.95
+- probability_immunity = 0.5
+- probability_vacc_infection = 0.03
+- probability_death = 0.05
+- probability_reinfection_death = 0.2
+- vaccinate_rate =  0.1 (***our low vaccination rate***)
+- community_spread = 0.02
+
+The full script for this trial and all parameters can be seen [here](https://gitlab.computing.dcu.ie/sweenk27/ca4024compmodels-assignment2/-/blob/master/experiments/low-vaccine-rate.py). Let's have a look at our outputs after approximately 100 steps of the simulation.
+
+![](images/low_vaccine_rate.png)
+
+
+### Experiment 2 - High Vaccination Rate
+
+The parameters we ran for this experiment starting off were the same as the previous experiment with the exception of:
+
+- vaccinate_rate =  0.9 (***high vaccination rate***)
+
+The full script for this can be found here [here](https://gitlab.computing.dcu.ie/sweenk27/ca4024compmodels-assignment2/-/blob/master/experiments/high-vaccine-rate.py). This was our output after 100 steps:
+
+![](images/high_vaccine_rate.png)
+
+Looking at our plot it can be hard to differentiate between some values, particularly at the lower end. Let's compare and contrast our values for deaths, infections, reinfections and vaccinations in our two experiments thus far for a better understanding. Click on the images below to focus in on them for a clearer view. Relevant code for this analysis can be found [here](https://gitlab.computing.dcu.ie/sweenk27/ca4024compmodels-assignment2/-/blob/master/data-analysis/high-vaccine/analysis-high-vaccine.ipynb). All analysis uses the same script like the one shown here for different scenarios. 
+
+High Vaccination Rate Stats Breakdown | Low Vaccination Stats Breakdown
+--- | ---
+![](data-analysis/high-vaccine/high-stats.png) | ![](data-analysis/low-vaccine/low-stats.png)
+
+Looking at the contrast in these key metrics we see the profound effect a high vaccine uptake has on a population. Looking at the number of deaths in the total population of 1000, we see the number of deaths after 100 steps at around 50 (and still rising) for a low vaccine uptake, whereas a high vaccine uptake sees deaths of approximately 30 which levels out after 80 steps and shows very little increases in this regard. There is also a notable impact on the number of reinfections in the population between these two scenarios. Both scenarios saw spikes of reinfections around the 30-40 step mark but there was a stark contrast in the scale of these spikes. A large vaccination rate saw a spike of approximately 65 active reinfections whereas the spike for a low vaccination rate saw a spike of approximately 150 active reinfections. Active infections see relatively similar patterns in peaks and falls, and similar scales to these numbers also. The difference in vaccine number is also huge here with over 600 being vaccinated by the 100th step for a high vaccination rate and approximately 80 for the same number of steps for a high vaccination rate. Without doubt our simulation here highlights the importance of effective vaccines here and vaccinating as many of the population that can be vaccinated quickly to prevent virus spread. This brings us to our next experiment.
+
+### Experiment 3 - Ineffective Vaccine
+
+With our previous experiments we had set the chance of infection when vaccinated to 0.03 (3% little to no chance). This implies an effective vaccine that protects agents well from exposure to the virus. With the coronavirus pandemic we saw exposure to multiple different vaccines such as Pfizer, Moderna, Astrazeneca and Janssen vaccines which all had differing efficacy rates in their clinical trials, some being more effective than others. Here I will test the impact of how a much higher chance of infection when vaccinated effects the population and see the impact it has on the same metrics we tracked before. We set our vaccination infection parameter like so:
+
+- probability_vacc_infection = 0.4
+
+The full script for this trial will be hosted [here](https://gitlab.computing.dcu.ie/sweenk27/ca4024compmodels-assignment2/-/blob/master/experiments/ineffective-vaccine.py). Here is a look at our outputs after approximately 100 steps:
+
+![](images/ineffective-vaccine.png)
+
+Let's show some of our key metrics again:
+
+![](data-analysis/ineffective-vaccine/ineffective-stats.png)
+
+Looking at this image compared to our results for experiment 2 we see the importance an effective vaccine has on preventing virus spread in a population. The difference in the number of deaths is stark here. There are over 50 deaths occurring here in the absence of an effective vaccine and looking at the graph for deaths here we see this trend is increasing steadily still. The numbers are very similar regarding deaths to a low vaccination rate, with a high effective vaccine (experiment 1) with the number of deaths seemingly more stark here than in experiment 1. The number of infections here is also higher, particularly toward the end of timeline covered presumably due to vaccinated agents encountering the virus and becoming infected. The number of reinfections is much smaller here than experiment 1 but this is most likely due to vaccinated agents becoming labelled as infected instead. The biggest takeaway here is despite an increasing trend of approximately 350 vaccinated agents after 100 steps in contrast to roughly 80 agents after a similar number of steps in our low vaccine rate experiment we see that the death rate can actually be higher in a population here with an ineffective vaccine being used on a population.
---
+++ experiments/low-vaccine-rate.py
@@ -0,0 +1,418 @@
+from re import T
+import pycxsimulator
+from pylab import *
+from random import sample
+import pandas as pd
+import os
+
+# Size of grid
+height = 1
+width = 1
+
+# Number of agents
+total_population = 1000
+
+# this is the probability of being infected by the virus and agent is not vaccinated/immune
+probability_infection = 0.95
+# probability of agent developing immunity after contracting the virus
+probability_immunity = 0.5
+# probability of infection if vaccinated
+probability_vacc_infection = 0.03
+
+## probability immunity after reinfection
+probability_immunity_reinfection = 0.95
+
+### these are our probabilities of death if infected and reinfected
+# probability of agent dying while infected with virus
+probability_death = 0.05
+# more likely to survive if reinfected
+probability_reinfection_death = 0.03
+
+## vaccination rate / can adjust slow or fast vaccination rate
+vaccinate_rate =  0.1
+
+# magnitude of movement
+community_spread = 0.02
+
+# Radius for collision detection
+cd = 0.02
+cdsq = cd ** 2
+
+# starting values, which will we will track
+deaths = 0
+# start with 10 infections in community
+infections = 10
+immune = 0
+vaccinated = 0
+reinfections = 0
+recovered = 0
+# if infected change healthy pop
+# don't need to take into account other states because once infected other states only come into play then
+# hence infections only need to be taken into account here
+healthy = total_population - infections
+
+# lists to track and plot relevant statistics over time
+healthy_lst = []
+deaths_lst = []
+infections_lst = []
+immune_lst = []
+vaccine_lst = []
+reinfection_lst = []
+rec_lst = []
+
+class agent:
+    pass
+
+def initialise():
+    global time, agents, deaths, infections, immune, vaccinated, reinfections, recovered, healthy
+
+    time = 0
+    agents = []
+    for i in range(total_population - infections):
+        ag = agent()
+        ag.state = 'healthy' if random() <= probability_infection else 'immune'
+        if ag.state == 'immune':
+            immune += 1
+            healthy -= 1
+            # time agent is immune from virus after having it, can be changed setting it at low value to start
+            ag.immunity_time = 10
+        ag.infected_time = -1
+        ag.vaccine_efficacy = -1
+        ag.x = uniform(0, width)
+        ag.y = uniform(0, height)
+        agents.append(ag)
+
+    # this is dealing with our initial collection of infections (total of 10)
+    # this initializes the infection time they have left, location etc
+    for i in range(infections):
+        ag = agent()
+        ag.state = 'infected'
+        # infection time left -> between 1 and 7
+        ag.infected_time = randint(1, 7)
+        # need to initialize these parameters or else code breaks
+        ag.immunity_time = 0
+        ag.vaccine_efficacy = 0
+        ag.x = uniform(0, width)
+        ag.y = uniform(0, height)
+        agents.append(ag)
+
+    ### initialise statistics we want to track and graph over time
+    healthy_lst.append([time, healthy])
+    deaths_lst.append([time, deaths])
+    infections_lst.append([time, infections])
+    immune_lst.append([time, immune])
+    vaccine_lst.append([time, vaccinated])
+    reinfection_lst.append([time, reinfections])
+    rec_lst.append([time, recovered])
+
+def observe():
+    global time, agents, deaths, infections, immune, vaccinated, reinfections, recovered, healthy
+    subplot(2, 2, 1)
+    cla()
+
+    infected = [ag for ag in agents if ag.state == 'infected']
+    if len(infected) > 0:
+        x = [ag.x for ag in infected]
+        y = [ag.y for ag in infected]
+        plot(x, y, 'r.')
+
+    immune_count = [ag for ag in agents if ag.state == 'immune']
+    if len(immune_count) > 0:
+        x = [ag.x for ag in immune_count]
+        y = [ag.y for ag in immune_count]
+        plot(x, y, 'g.')
+
+    # Plotting healthy (not infected) (black)
+    healthys = [ag for ag in agents if ag.state == 'healthy']
+    if len(healthys) > 0:
+        x = [ag.x for ag in healthys]
+        y = [ag.y for ag in healthys]
+        plot(x, y, 'k.')
+    
+    # Plotting vaccinated (magenta , none to start)
+    vaccine = [ag for ag in agents if ag.state == 'vaccinated']
+    if len(vaccine) > 0:
+        x = [ag.x for ag in vaccine]
+        y = [ag.y for ag in vaccine]
+        plot(x, y, 'm.')
+
+    reinf = [ag for ag in agents if ag.state == 'reinfected']
+    if len(reinf) > 0:
+        x = [ag.x for ag in reinf]
+        y = [ag.y for ag in reinf]
+        plot(x, y, 'c.')
+
+    recover = [ag for ag in agents if ag.state == 'recovered']
+    if len(recover) > 0:
+        x = [ag.x for ag in recover]
+        y = [ag.y for ag in recover]
+        plot(x, y, 'b.')
+
+    axis('image')
+    axis([0, 1, 0, 1])
+
+    ax = subplot(2, 1, 2)
+    cla()
+    plot(deaths_lst, color = 'crimson', label = 'deaths')
+    # weird glitch was duplicating lines on plot so remove duplicates
+    # these lines were in set positions at top of the plot also
+    ax.lines[0].remove()
+    plot(vaccine_lst, color = 'gold', label = 'vaccines')
+    ax.lines[1].remove()
+    plot(infections_lst, color = 'lightsalmon', label = 'infections')
+    ax.lines[2].remove()
+    plot(immune_lst, color = 'violet', label = 'immune')
+    ax.lines[3].remove()
+    plot(reinfection_lst, color = 'black', label = 'reinfection')
+    ax.lines[4].remove()
+    plot(rec_lst, color = 'dodgerblue', label = 'recovered')
+    ax.lines[5].remove()
+    plot(healthy_lst, color = 'lime', label = 'healthy')
+    ax.lines[6].remove()
+    legend()
+
+def update_one_agent():
+    global time, agents, deaths, infections, immune, vaccinated, reinfections, recovered, healthy
+
+    if len(agents) == 0:
+        return
+
+    ag = choice(agents)
+
+    ag.x += uniform(-community_spread, community_spread)
+    ag.y += uniform(-community_spread, community_spread)
+    ag.x = 1 if ag.x > 1 else 0 if ag.x < 0 else ag.x
+    ag.y = 1 if ag.y > 1 else 0 if ag.y < 0 else ag.y
+
+    # Detecting collisions
+    neighbours = [nb for nb in agents if (ag.x - nb.x) ** 2 + (ag.y - nb.y) ** 2 < cdsq]
+
+    # If current agent is infected, all neighbours become infected unless immune or vaccinated
+    if ag.state == 'infected':
+        for nb in neighbours:
+            if nb.state == 'healthy':
+                nb.state = 'infected'
+                nb.infected_time = 0
+                nb.immunity_time = 0
+                nb.vaccine_efficacy = 0
+                infections += 1
+                healthy -= 1
+            elif nb.state == 'recovered':
+                nb.state = 'reinfected'
+                nb.infected_time = 0
+                nb.immunity_time = 0
+                nb.vaccine_efficacy = 0
+                reinfections += 1
+            else:
+                continue
+
+    elif ag.state == 'reinfected':
+        for nb in neighbours:
+            if nb.state == 'healthy':
+                nb.state = 'infected'
+                nb.infected_time = 0
+                nb.immunity_time = 0
+                nb.vaccine_efficacy = 0
+                infections += 1
+                healthy -= 1
+            elif nb.state == 'recovered':
+                nb.state = 'reinfected'
+                nb.infected_time = 0
+                nb.immunity_time = 0
+                nb.vaccine_efficacy = 0
+                reinfections += 1
+            else:
+                continue
+
+    # If current agent is healthy, it will become infected if any neighbours are infected
+    elif ag.state == 'healthy':
+        infected_neighbours = [ag for ag in neighbours if ag.state == 'infected']
+        reinfected_neighbours = [ag for ag in neighbours if ag.state == 'reinfected']
+        if len(infected_neighbours) > 0 or len(reinfected_neighbours) > 0:
+            ag.state = 'infected'
+            ag.infected_time = 0
+            ag.immunity_time = 0
+            ag.vaccine_efficacy = 0
+            infections += 1
+            healthy -= 1
+
+    # handle reinfected case    
+    elif ag.state == 'recovered':
+        infected_neighbours = [ag for ag in neighbours if ag.state == 'infected']
+        reinfected_neighbours = [ag for ag in neighbours if ag.state == 'reinfected']
+        if len(infected_neighbours) > 0 or len(reinfected_neighbours) > 0:
+            ag.state = 'reinfected'
+            ag.infected_time = 0
+            ag.immunity_time = 0
+            ag.vaccine_efficacy = 0
+            reinfections += 1
+            recovered -= 1
+        
+    elif ag.state == 'vaccinated':
+        # small chance of infection if vaccinated
+        infected_neighbours = [ag for ag in neighbours if ag.state == 'infected']
+        reinfected_neighbours = [ag for ag in neighbours if ag.state == 'reinfected']
+        if len(infected_neighbours) > 0 or len(reinfected_neighbours) > 0:
+            if random() <= probability_vacc_infection:
+                ag.state = 'infected'
+                ag.infected_time = 0
+                ag.immunity_time = -1
+                ag.vaccine_efficacy = -1
+                infections += 1
+                vaccinated -= 1
+            else:
+                pass
+        else:
+            pass
+
+    # If current agent is immune ignore
+    else:
+        pass
+
+def update():
+    global time, agents, infections, deaths, immune, vaccinated, healthy, recovered, reinfections
+
+    # increase infection time over time
+    infected = [ag for ag in agents if ag.state == 'infected']
+    for ag in infected:
+        ag.infected_time += 1
+
+    # decrease immunity over time
+    immun = [ag for ag in agents if ag.state == 'immune']
+    for ag in immun:
+        ag.immunity_time -= 1
+
+    # decrease vaccine efficacy over time
+    vacc = [ag for ag in agents if ag.state == 'vaccinated']
+    for ag in vacc:
+        ag.vaccine_efficacy -= 1
+    
+    # increase time reinfected after each step
+    reinfs = [ag for ag in agents if ag.state == 'reinfected']
+    for ag in reinfs:
+        ag.infected_time += 1
+
+    ## randomly vaccinate portion of population that has not contracted the virus
+    healthys = [ag for ag in agents if ag.state == 'healthy']
+    # take random sample of healthy agents
+    if len(healthys) > 10:
+        r = len(healthys) % 10
+        sample1 = sample(healthys, r)
+    else:
+        sample1 = healthys
+    for ag in sample1:
+        if random() < vaccinate_rate:
+            ag.state = 'vaccinated'
+            # set timeframe on vaccine effectiveness
+            ag.vaccine_efficacy = 250
+            vaccinated += 1
+            healthy -= 1
+        else:
+            continue
+
+    # Change agents who have been infected for 14 days to either immune/recovered
+    infection_over = [ag for ag in agents if ag.infected_time == 14 and ag.state == 'infected']
+    # Subtract number of recovered from current infections
+    infections -= len(infection_over)
+    for ag in infection_over:
+        # Random chance of death after infection
+        if random() <= probability_death:
+            agents.remove(ag)
+            # Add to deaths total
+            deaths += 1
+        ag.infected_time = -1
+
+        # Change to immune if greater than or equal random prob
+        if random() <= probability_immunity:
+            ag.state = 'immune'
+            # set time of immunity
+            ag.immunity_time = 30
+            ag.infected_time = -1
+            # Add to immune total
+            immune += 1
+        # if not immune label as recovered so can track reinfection if it occurs
+        else:
+            ag.state = 'recovered'
+            ag.infected_time = -1
+            recovered += 1
+
+    # Change agents who have been reinfected for 14 days to immune if they survive
+    reinfection_over = [ag for ag in agents if ag.infected_time == 14 and ag.state == 'reinfected']
+    # Subtract number of recovered from current reinfections
+    reinfections -= len(reinfection_over)
+    for ag in reinfection_over:
+        # Random chance of death after reinfection (higher rate this time)
+        if random() <= probability_reinfection_death:
+            agents.remove(ag)
+            # Add to deaths total
+            deaths += 1
+        ag.infection_time = -1
+        
+        if random() <= probability_immunity_reinfection:
+            ag.state = 'immune'
+        # high number as we want no real chance of reinfection after being reinfected once
+            ag.immunity_time = 365
+            immune += 1
+        else:
+            ag.state = 'recovered'
+
+    # take random sample of recovered agents and vaccinate
+    recoveries = [ag for ag in agents if ag.state == 'recovered']
+    if len(recoveries) > 10:
+        r1 = len(recoveries) % 10
+        sample2 = sample(recoveries, r1)
+    else:
+        sample2 = recoveries
+    for ag in sample2:
+        if random() < vaccinate_rate:
+            ag.state = 'vaccinated'
+            # set timeframe on vaccine effectiveness, longer here to previous infection
+            ag.vaccine_efficacy = 360
+            vaccinated += 1
+            recovered -= 1
+        else:
+            continue
+    
+    # Change agents who's immunity has waned to at risk again (healthy)
+    for ag in immun:
+        # if agents immunity from virus is gone change state
+        if ag.immunity_time == 0:
+            ag.state = 'recovered'
+            # remove agent from count of immune
+            immune -= 1
+            recovered += 1
+        else:
+            continue
+    
+    for ag in vacc:
+        # if agents vaccine efficacy timeline has passed change state
+        # change to healthy as they needed to be healthy to get vaccine at start
+        if ag.vaccine_efficacy == 0:
+            ag.state = 'healthy'
+            healthy += 1
+        else:
+            continue
+
+    sample_move = total_population * 0.5
+    sample_move = round(sample_move)
+    for i in range(sample_move):   
+        update_one_agent()
+
+    # Collecting statistics over time for subplot of statistics over time
+    healthy_lst.append([time, healthy])
+    deaths_lst.append([time, deaths])
+    infections_lst.append([time, infections])
+    immune_lst.append([time, immune])
+    vaccine_lst.append([time, vaccinated])
+    reinfection_lst.append([time, reinfections])
+    rec_lst.append([time, recovered])
+
+pycxsimulator.GUI().start(func=[initialise, observe, update])
+
+#### we have tuples in our lists here need to get into csv format to analyse better
+#### Creates a DataFrame object from a structured ndarray, sequence of tuples (pd.DataFrame.from_records)
+
+pd.DataFrame.from_records(deaths_lst, columns=['time', 'deaths']).to_csv(os.path.join('./data-analysis/low-vaccine/', 'deaths.csv'), index=False)
+pd.DataFrame.from_records(infections_lst, columns=['time', 'infections']).to_csv(os.path.join('./data-analysis/low-vaccine/', 'infections.csv'), index=False)
+pd.DataFrame.from_records(vaccine_lst, columns=['time', 'vaccinated']).to_csv(os.path.join('./data-analysis/low-vaccine/', 'vaccine.csv'), index=False)
+pd.DataFrame.from_records(reinfection_lst, columns=['time', 'reinfections']).to_csv(os.path.join('./data-analysis/low-vaccine/', 'reinfections.csv'), index=False)
